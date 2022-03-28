0a5eac9f699dfc16168b168605657193232e761b
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-03-28 11:28:51 -0400

    Initial logic of switching between component views displayed on page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe_book_app';
+  private recipesShown: boolean;
+
+  constructor(){
+    this.recipesShown = true;
+  }
+
+  onPageToggle(e: boolean){
+    this.recipesShown = e;
+  }
+
+  show(){
+    return this.recipesShown;
+  }
 }
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() toggleRequest = new EventEmitter<boolean>();
 
   collapsed = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  togglePage(d: string){
+    const condition = d === 'recipes';
+    this.toggleRequest.emit(
+      condition
+    );
   }
 
 }
