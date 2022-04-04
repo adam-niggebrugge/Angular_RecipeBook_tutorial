05751e5768918a4b6b9337d89cf51db9ac47d9d9
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-04-04 10:25:42 -0400

    Demo that data CAN be passed through multiple parents. Might not always be best. More elegant solution later

--- src/app/component/recipes/recipe-list/recipe-list.component.ts
+++ src/app/component/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { Recipe } from "../../../models/recipe.model";
 
 @Component({
@@ -11,10 +11,15 @@ export class RecipeListComponent implements OnInit {
     new Recipe("Test Recipe", "This is simply a test", "https://publicdomainrecipes.org/recipes/stewed-carrots/Stewed%20Carrots_hud045b9506b44f73973a028a04f65a9ab_68762_350x350_fill_q75_box_smart1.jpg")
   ];
 
+  @Output() RecipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.recipes)
   }
 
+  onRecipeSelected(clickedRecipe: Recipe){
+    this.RecipeWasSelected.emit(clickedRecipe);
+  }
 }
