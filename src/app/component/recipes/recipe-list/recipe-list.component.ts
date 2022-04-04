e7997eb1cedd2a6738c1c79cd3cbe2732b25dc68
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-04-04 10:43:57 -0400

    Filled in detail html to take parts of model. Additional test value added to list.

--- src/app/component/recipes/recipe-list/recipe-list.component.ts
+++ src/app/component/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from "../../../models/recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe("Test Recipe", "This is simply a test", "https://publicdomainrecipes.org/recipes/stewed-carrots/Stewed%20Carrots_hud045b9506b44f73973a028a04f65a9ab_68762_350x350_fill_q75_box_smart1.jpg")
+    new Recipe("Test Recipe", "This is simply a test", "https://publicdomainrecipes.org/recipes/stewed-carrots/Stewed%20Carrots_hud045b9506b44f73973a028a04f65a9ab_68762_350x350_fill_q75_box_smart1.jpg"),
+    new Recipe("Test Recipe Deux", "This is simply a test with a different name and value. Picture stays the same", "https://publicdomainrecipes.org/recipes/stewed-carrots/Stewed%20Carrots_hud045b9506b44f73973a028a04f65a9ab_68762_350x350_fill_q75_box_smart1.jpg")
   ];
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
