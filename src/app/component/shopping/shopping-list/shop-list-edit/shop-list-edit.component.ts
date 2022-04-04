64e1492224cca2558fe4c698d172952ce86b31b0
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-04-04 11:00:31 -0400

    Filling in form to add to an array of self defined values

--- src/app/component/shopping/shopping-list/shop-list-edit/shop-list-edit.component.ts
+++ src/app/component/shopping/shopping-list/shop-list-edit/shop-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/models/ingredients.model';
 
 @Component({
   selector: 'app-shop-list-edit',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop-list-edit.component.css']
 })
 export class ShopListEditComponent implements OnInit {
+  @ViewChild("nameInput", {static: true}) nameInputRef: ElementRef;
+  @ViewChild("amountInput", {static: true}) amountInputRef: ElementRef;
+  @ViewChild("detailInput", {static: true}) detailInputRef: ElementRef;
 
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const ingDetail = this.detailInputRef.nativeElement.value;
+
+    const newIngredient = new Ingredient(ingName, ingDetail, ingAmount);
+
+    this.ingredientAdded.emit(newIngredient);
+    
+  }
 }
