560cdb29407c7e400941accca7f23d02843ba88c
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-06-02 11:38:33 -0400

    REVISED linking ingredients to recipes and displaying mock data

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../models/ingredients.model";
 
 import { Recipe } from "../models/recipe.model";
 
@@ -6,8 +7,18 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe("Test Recipe", "This is simply a test", "https://publicdomainrecipes.org/recipes/stewed-carrots/Stewed%20Carrots_hud045b9506b44f73973a028a04f65a9ab_68762_350x350_fill_q75_box_smart1.jpg"),
-        new Recipe("Test Recipe Deux", "This is simply a test with a different name and value. Picture stays the same", "https://publicdomainrecipes.org/recipes/stewed-carrots/Stewed%20Carrots_hud045b9506b44f73973a028a04f65a9ab_68762_350x350_fill_q75_box_smart1.jpg")
+        new Recipe("Test Recipe", "This is simply a test", "https://publicdomainrecipes.org/recipes/stewed-carrots/Stewed%20Carrots_hud045b9506b44f73973a028a04f65a9ab_68762_350x350_fill_q75_box_smart1.jpg",
+        [
+            new Ingredient('Carrot', 'Sweet root vegetable', 12),
+            new Ingredient('Sugar', 'Raw granular sweetner', 2),
+            new Ingredient('Parsley', 'Leafy herb that adds bitters', 5)
+        ]),
+        new Recipe("Test Recipe Deux", "This is simply a test with a different name and value. Picture stays the same", "https://publicdomainrecipes.org/recipes/stewed-carrots/Stewed%20Carrots_hud045b9506b44f73973a028a04f65a9ab_68762_350x350_fill_q75_box_smart1.jpg",
+        [
+            new Ingredient('Parsnips', 'Sweet root vegetable', 12),
+            new Ingredient('Salt', 'A preservative', 2),
+            new Ingredient('Cilantro', 'Leafy herb that has a citrus like taste', 5)
+        ])
       ];
     
     getRecipes() {
